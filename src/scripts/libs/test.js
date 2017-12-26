1da09b3aa04b1a614bf6f15bfc4f7718d355ad17
David Straka <32021723+davidstraka2@users.noreply.github.com>
2017-12-26 07:21:52 +0100

    Set up test js files

---
+++ src/scripts/libs/test.js
@@ -0,0 +1,5 @@
+const a = () => console.log('a2');
+const b = () => console.log('b2');
+const c = () => console.log('c2');
+
+export {a, b, c};
---
+++ src/scripts/main/main.js
@@ -0,0 +1,6 @@
+import * as lib from '../libs/test';
+import {a, c} from '../_modules-babel/test';
+
+a();
+lib.a();
+lib.c();
