90415861dd9d7d78f8c4f0e00f11c2b785e8b9a1
David Straka <32021723+davidstraka2@users.noreply.github.com>
2018-03-11 20:54:00 +0100

    Tweaked jpeg scaling

--- node/optimize-jpeg/optimize-jpeg.js
+++ node/optimize-jpeg/optimize-jpeg.js
@@ -3,6 +3,25 @@ const guetzli = require('imagemin-guetzli');
 const imagemin = require('imagemin');
 const jimp = require('jimp');
 
+const aspectRatio = (a, b) => {
+    const gcd_ = gcd(a, b);
+    return [a / gcd_, b / gcd_];
+};
+
+// Greatest common divisor
+const gcd = (numerator, denominator) => denominator ? gcd(denominator,
+    numerator % denominator) : numerator;
+
+const lowerRes = (inFile, outFile, maxRes, callback) => jimp.read(inFile)
+    .then(img => {
+        const targetWidth = scaleDown(
+            [img.bitmap.width, img.bitmap.height],
+            maxRes,
+        );
+        img.resize(targetWidth, jimp.AUTO);
+        img.write(outFile, callback);
+    }).catch(err => console.log(err));
+
 const optimize = (inFile, outFile, outDir) => imagemin([outFile], outDir, {
     use: [
         guetzli({quality: 85}),
@@ -10,27 +29,25 @@ const optimize = (inFile, outFile, outDir) => imagemin([outFile], outDir, {
 }).then(() => console.log(`Optimized ${ inFile } to ${ outFile }`))
     .catch(err => console.log(err));
 
-const downsize = (inFile, outFile, maxHeight, maxWidth, callback) => jimp
-    .read(inFile)
-    .then(img => {
-        if (img.bitmap.height > img.bitmap.width)
-            img.resize(maxWidth, jimp.AUTO);
-        else
-            img.resize(jimp.AUTO, maxHeight);
-        img.write(outFile, callback);
-    }).catch(err => console.log(err));
+const scaleDown = (dimensions, maxRes) => {
+    if (dimensions[0] * dimensions[1] < maxRes)
+        return dimensions[0];
+    const aspectRatio_ = aspectRatio(...dimensions);
+    return Math.floor(Math.sqrt(aspectRatio_[0] * maxRes / aspectRatio_[1]));
+};
 
 const go = cfg => globby(cfg.input)
     .then(files => files.forEach(file => {
         const outFile = cfg.getOutput(file);
-        downsize(file, outFile, cfg.maxHeight, cfg.maxWidth, optimize
-            .bind(null, file, outFile, cfg.getOutputDir(outFile)));
+        lowerRes(
+            file, outFile, cfg.maxRes,
+            optimize.bind(null, file, outFile, cfg.getOutputDir(outFile)),
+        );
     })).catch(err => console.log(err));
 
 go({
     input: './node/optimize-jpeg/input/**/*.{jpg,jpeg}',
-    maxHeight: 1080,
-    maxWidth: 1920,
+    maxRes: 1920 * 1080,
     getOutput: filepath => filepath.replace('input', 'output'),
     getOutputDir: filepath => filepath.substr(0, filepath.lastIndexOf('/')),
 });
